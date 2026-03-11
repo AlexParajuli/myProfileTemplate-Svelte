0a8f5263e3e399d93258b8ac48c6793d09636268
AlexParajuli <sc54040@info.sangitan.ac.jp>
2026-03-11 20:40:03 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-const production = process.env.NODE_ENV === 'production';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html' // すべてのリクエストを index.html にフォールバック
-		}),
-		paths: {
-            base: production ? '/myProfileTemplate-Svelte' : '',
-        }
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs20.x'  // 👈 add this
+    })
+  }
 };
 
 export default config;
